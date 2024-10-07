1c5ea6832d4956bec5e3eac4a188e512c4e72f00
Nikolai Shalakin <nikolai.shalakin@lifeisfeudal.com>
2024-10-07 22:18:58 +0200

    try to eliminate wasm warning

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,6 +1,2 @@
-use crate::ieee754_ops::{binary_to_decimal, binary_to_decimal_ext, BinaryInfo, FLOAT32_LAYOUT};
-use ieee754_ops::decimal_to_binary;
-use wasm_bindgen::prelude::*;
-
 mod bitfield;
 mod ieee754_ops;
